191e58d4c0b4a7212efc8765fef9d06bdcf515b7
akh820 <akh8202@gmail.com>
2025-05-02 20:49:49 +0900

    feat: 커밋 테스트

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <header className="sticky top-0 z-50 border-b">
         <div className="container mx-auto flex h-14 items-center px-4">
           <a href="#" className="text-xl font-semibold">
-            <span className="font-bold">Hongblog</span>
+            <span className="font-bold">Hong's blog</span>
           </a>
           <nav className="ml-auto flex items-center gap-4">
             <a href="#" className="hover:text-primary font-medium">
