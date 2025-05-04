24aae1f7a1df3aadb4ac37e52a8e8930b60d3a66
akh820 <akh8202@gmail.com>
2025-05-04 14:42:55 +0900

    feat: blog 관련 UI 변경

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <main className="flex-1">
+      <div className="container py-8">
+        <div className="space-y-8">
+          {/* 섹션 제목 */}
+          <h2 className="text-3xl font-bold tracking-tight">소개</h2>
+        </div>
+      </div>
+    </main>
+  );
+}
