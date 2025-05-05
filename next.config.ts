d84b6ea377af22e51d699c62f51af1a7bfd16660
akh820 <akh8202@gmail.com>
2025-05-05 18:09:13 +0900

    feat: 배포 전 오류 수정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
-import remarkGfm from 'remark-gfm';
-//공식문서 참조
 import createMDX from '@next/mdx';
+// import remarkGfm from 'remark-gfm';
+
+console.log('process.env.NODE_ENV:', process.env.NODE_ENV);
+console.log('process.env.ENV_NAME:', process.env.ENV_NAME);
+console.log('process.env.LOCAL_ENV_NAME:', process.env.LOCAL_ENV_NAME);
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -24,14 +27,12 @@ const nextConfig: NextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'mdx', 'md'],
 };
 
-//next js 설정과 병합
 const withMDX = createMDX({
-  //필요한 마크다운 플러그인 추가
+  // 필요한 마크다운 플러그인을 추가할 수 있음~!
   options: {
-    remarkPlugins: [['remark-gfm']], //turbopack 실행시
-    // remarkPlugins: [remarkGfm], // webpack실행시
-    // @ts-expect-error remark-gfm 타입 충돌 문제 해결
-    // rehypePlugins: [['rehype-katex', { strict: true, throwOnError: true }]],
+    // remarkPlugins: [remarkGfm],
+    // ts-expect-error remark-gfm 타입 충돌 문제 해결
+    // remarkPlugins: [['remark-gfm']],
   },
 });
 
