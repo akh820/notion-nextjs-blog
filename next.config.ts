90b7f722b30785fdbc837b9093e60dd0da520b81
akh820 <akh8202@gmail.com>
2025-05-05 17:41:14 +0900

    feat: 목차 생성

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,7 @@
 import type { NextConfig } from 'next';
+import remarkGfm from 'remark-gfm';
+//공식문서 참조
+import createMDX from '@next/mdx';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -18,6 +21,18 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'mdx', 'md'],
 };
 
-export default nextConfig;
+//next js 설정과 병합
+const withMDX = createMDX({
+  //필요한 마크다운 플러그인 추가
+  options: {
+    remarkPlugins: [['remark-gfm']], //turbopack 실행시
+    // remarkPlugins: [remarkGfm], // webpack실행시
+    // @ts-expect-error remark-gfm 타입 충돌 문제 해결
+    // rehypePlugins: [['rehype-katex', { strict: true, throwOnError: true }]],
+  },
+});
+
+export default withMDX(nextConfig);
